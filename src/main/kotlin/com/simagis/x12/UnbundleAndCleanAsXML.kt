aea6bf12e2507e9aa4f4127dabd52af232d6857d
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2017-01-24 15:11:23 +0500

    initial commit

---
+++ src/main/kotlin/com/simagis/x12/UnbundleAndCleanAsXML.kt
@@ -0,0 +1,6 @@
+package com.simagis.x12
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/simagis/x12/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.simagis.x12
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
